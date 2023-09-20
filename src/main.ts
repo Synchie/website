71564f302260248915ff5767aa335b27a6b5fcf9
Matija Achterberg <matija.achterberg@gmail.com>
2023-09-20 20:29:53 +0200

    Added transloco for i18n

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,29 @@
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { AppComponent } from './app/components/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { heroArrowTopRightOnSquare } from '@ng-icons/heroicons/outline';
 import { provideIcons } from '@ng-icons/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { getBrowserLang, provideTransloco } from '@ngneat/transloco';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule, AppRoutingModule, HttpClientModule),
     provideIcons({ heroArrowTopRightOnSquare }),
     provideAnimations(),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'de'],
+        defaultLang: getBrowserLang(),
+        fallbackLang: 'en',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 }).catch((err) => console.error(err));
