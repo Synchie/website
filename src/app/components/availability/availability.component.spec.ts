2a15dec3d85b12c6e4b95fe76fc2291e199c62c8
Matija Achterberg <matija.achterberg@gmail.com>
2023-09-23 20:18:14 +0200

    added availability component

---
+++ src/app/components/availability/availability.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AvailabilityComponent } from './availability.component';
+
+describe('AvailabilityComponent', () => {
+  let component: AvailabilityComponent;
+  let fixture: ComponentFixture<AvailabilityComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AvailabilityComponent]
+    });
+    fixture = TestBed.createComponent(AvailabilityComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/availability/availability.component.ts
@@ -0,0 +1,49 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslocoModule } from '@ngneat/transloco';
+
+@Component({
+  selector: 'app-availability',
+  standalone: true,
+  imports: [CommonModule, TranslocoModule],
+  templateUrl: './availability.component.html',
+})
+export class AvailabilityComponent implements AfterViewInit {
+  @ViewChild('canvas') canvas?: ElementRef<HTMLCanvasElement>;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.drawOnCanvas();
+  }
+
+  drawOnCanvas() {
+    const canvas = this.canvas?.nativeElement;
+    const ctx = canvas?.getContext('2d');
+    console.log(canvas!.width);
+    console.log(canvas!.title);
+
+    this.fillReact('red', 0, canvas!.height, canvas!.width, ctx);
+    this.fillReact('green', 250, canvas!.height, canvas!.width, ctx);
+  }
+
+  private fillReact(
+    color: 'green' | 'red',
+    start: number,
+    height: number,
+    width: number,
+    ctx: CanvasRenderingContext2D | null | undefined,
+    end: number = 0
+  ) {
+    if (!ctx?.fillStyle) return;
+
+    ctx.fillStyle = color;
+    ctx.fillRect(start, end, width, height);
+  }
+}
